bb66680d240e19c955dcf93d39a6b3c07b1f5a1f
Joshua <Joshua@DESKTOP-40SHIQI>
2025-03-15 04:39:28 -0400

    2 axis's created

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,36 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode({1200, 800}), "SFML works!");
+
+
+    
+    std::array lineX = 
+    {
+        sf::Vertex{sf::Vector2f(500.f,400.f)},
+        sf::Vertex{sf::Vector2f(800.f,400.f)}
+    };
+
+    std::array lineY = {
+        sf::Vertex{sf::Vector2f(500.f,100.f)},
+        sf::Vertex{sf::Vector2f(500.f,400.f)}
+    };
+
+
+
 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
+
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
 
         window.clear();
+        window.draw(lineX.data(), lineX.size(), sf::PrimitiveType::Lines);
+        window.draw(lineY.data(), lineY.size(), sf::PrimitiveType::Lines);
         window.display();
     }
 }
